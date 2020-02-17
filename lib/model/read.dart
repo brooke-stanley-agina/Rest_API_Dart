08b99c3983d9cfed89fd145df0d8d68d70d11862
root <root@localhost.localdomain>
2020-02-17 09:08:37 +0300

    new reads

--- lib/model/read.dart
+++ lib/model/read.dart
@@ -1,23 +1,7 @@
 import 'package:my_app/my_app.dart';
 
-class Read extends Serializable{
+class _Read{
   String title;
   String author;
   int year;
-
-  @override
-  Map<String, dynamic> asMap() => {
-       'title': title,
-       'author': author,
-       'year': year
-     };
-  
-
-  @override
-  void readFromMap(Map<String, dynamic> object){
-   title = object['title'] as String;
-   author = object['author'] as String;
-   year = object['title'] as int;
-  }
-
 }
